4c736d1470cb81c82d627c846ef016c9e5be1bca
Andres Muñoz <andrefe91@gmail.com>
2024-09-12 15:09:13 -0500

    LeaderBoard page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,23 +2,39 @@
 import React from "react";
 
 export default function About() {
-    return (
-        <>
-            <div className="d-flex  align-items-center">
-                <div className="card border border-primary border-3 shadow" style={{width: "calc(min(90vw, 40rem))", height: "h-30", borderRadius: "1rem"}}>
-                    <div className="card-header text-center fs-2" style={{backgroundColor: "#424242", color: "#f5f5f5"}}>About</div>
-                    <div className="card-body">
-                        <p className="fs-3">
-                            This application was made as a project for The Odin Project using React, Ruby on Rails and some basic design concepts deployed on the Fly.io platform. The art belongs to:
-                        </p>
+	return (
+		<>
+			<div className="d-flex  align-items-center">
+				<div
+					className="card border border-primary border-3 shadow"
+					style={{
+						width: "calc(min(90vw, 40rem))",
+						height: "h-30",
+						borderRadius: "1rem",
+					}}
+				>
+					<div
+						className="card-header text-center fs-2"
+						style={{ backgroundColor: "#424242", color: "#f5f5f5" }}
+					>
+						About
+					</div>
+					<div className="card-body">
+						<p className="fs-3">
+							This application was made as a project for The Odin Project using
+							React, Ruby on Rails and some basic design concepts deployed on
+							the Fly.io platform. The art belongs to:
+						</p>
 
-                        <div className="d-flex justify-content-center fs-3">
-                            <a href="https://elijahhaswell.myportfolio.com" >Elijah Haswell</a>
-                            <a href="https://anomaly-world.com/" className="ms-4">Anomaly World Studio</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+						<div className="d-flex justify-content-center fs-3">
+							<a href="https://elijahhaswell.myportfolio.com">Elijah Haswell</a>
+							<a href="https://anomaly-world.com/" className="ms-4">
+								Anomaly World Studio
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
--- src/components/LeaderBoard/LeaderBoard.jsx
+++ src/components/LeaderBoard/LeaderBoard.jsx
@@ -2,9 +2,43 @@
 import React from "react";
 
 export default function LeaderBoard() {
-    return (
-        <>
-            <h2>Hello from the LeaderBoard component</h2>
-        </>
-    )
+	return (
+		<>
+			<div
+				className="d-flex flex-column justify-content-center"
+				style={{ width: "calc(min(90vw, 40rem))" }}
+			>
+				<table className="table fs-3 table-striped table-hover table-bordered table-fixed border-primary border-3 shadow">
+					<thead className="table-primary text-center">
+						<tr >
+							<th scope="col">#</th>
+							<th scope="col">Name</th>
+							<th scope="col">Time</th>
+							<th scope="col">Date</th>
+						</tr>
+					</thead>
+					<tbody className="table-group-divider">
+						<tr className="">
+							<td scope="row">1</td>
+							<td>Item</td>
+							<td>Item</td>
+							<td>Item</td>
+						</tr>
+						<tr className="">
+							<td scope="row">2</td>
+							<td>Item</td>
+							<td>Item</td>
+							<td>Item</td>
+						</tr>
+						<tr className="">
+							<td scope="row">3</td>
+							<td>Item</td>
+							<td>Item</td>
+							<td>Item</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
 }
